f12728469913250397e991e894ab2569490080b4
Omar-Goba <omaryasserassem1@gmail.com>
2025-05-12 01:04:51 +0300

    Feat: add bioler plate code for train

---
+++ src/train.py
@@ -0,0 +1,19 @@
+def main() -> int:
+    """
+    do the following:
+    1. load the dataset
+    2. construct the model
+        - n_lstm layers with the correct configs
+        - n_dense layers
+        - (i need dropout layer between every lstm)
+        - (i need last lstm to return sequences)
+    3. compile the model
+        - loss function
+        - optimizer
+        - metrics
+    4. train the model
+    5. test the model
+    6. show graph of history
+    7. save the model
+    """
+    ...
